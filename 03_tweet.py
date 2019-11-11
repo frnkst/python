1d2c8406a7081529ec515b7c074c5dbbd21e89d3
Frank Stöckli <f@void0.ch>
2019-11-11 21:58:18 +0100

    Refactoring

--- 03_tweet.py
+++ 03_tweet.py
@@ -7,11 +7,12 @@ import sys
 
 config = configparser.ConfigParser()
 config.read('twitter-oauth.ini')
+oauth = config['oauth']
 
 t = Twitter(auth=OAuth(
-    config['oauth']['token'],
-    config['oauth']['token_secret'],
-    config['oauth']['consumer_key'],
-    config['oauth']['consumer_secret'],
+    oauth['token'],
+    oauth['token_secret'],
+    oauth['consumer_key'],
+    oauth['consumer_secret'],
 ))
 t.statuses.update(status=sys.argv[1])
